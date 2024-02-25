4751b5d5ef5b3854c32e5187d6387901d9afb2b4
valorant-dhruv <78591597+valorant-dhruv@users.noreply.github.com>
2024-02-26 01:20:10 +0530

    changes to make it compatible with fireproof

--- ondisconnect/app.js
+++ ondisconnect/app.js
@@ -1,29 +1,70 @@
-// Copyright 2018-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
+/*
+  Copyright 2018-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+  SPDX-License-Identifier: MIT-0
+  Copyright 2024 Fireproof Storage Incorporated. All Rights Reserved.
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this
+  software and associated documentation files (the "Software"), to deal in the Software
+  without restriction, including without limitation the rights to use, copy, modify,
+  merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+"use strict";
+import AWS from "aws-sdk";
+import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+  PutCommand,
+  GetCommand,
+  DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
 
-// https://docs.aws.amazon.com/apigateway/latest/developerguide/apigateway-websocket-api-route-keys-connect-disconnect.html
-// The $disconnect route is executed after the connection is closed.
-// The connection can be closed by the server or by the client. As the connection is already closed when it is executed, 
-// $disconnect is a best-effort event. 
-// API Gateway will try its best to deliver the $disconnect event to your integration, but it cannot guarantee delivery.
+AWS.config.update({ region: process.env.AWS_REGION });
+const client = new DynamoDBClient({});
+const dynamo = DynamoDBDocumentClient.from(client);
+const tableName = process.env.TABLE_NAME;
 
-const AWS = require('aws-sdk');
+export const handler = async (event) => {
+  return await deleteSubscribers(event).catch((error) => {
+    console.error("Error:", error);
+    return {
+      status: 500,
+      body: JSON.stringify({
+        message: "Internal Server Error",
+        error: error.message,
+      }),
+    };
+  });
+};
 
-const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
-
-exports.handler = async event => {
-  const deleteParams = {
-    TableName: process.env.TABLE_NAME,
-    Key: {
-      connectionId: event.requestContext.connectionId
-    }
-  };
-
+async function deleteSubscribers(event) {
+  console.log("This is the route key", event.requestContext.routeKey);
+  // const { queryStringParameters } = event;
+  // const database = queryStringParameters.database;
+  console.log("This is the request context", event.requestContext);
   try {
-    await ddb.delete(deleteParams).promise();
+    await dynamo.send(
+      new DeleteCommand({
+        TableName: tableName,
+        Key: {
+          connectionId: event.requestContext.connectionId,
+          // databasename: database,
+        },
+      })
+    );
   } catch (err) {
-    return { statusCode: 500, body: 'Failed to disconnect: ' + JSON.stringify(err) };
+    console.log("This is the error", err);
+    return {
+      statusCode: 500,
+      body: "Failed to disconnect: " + JSON.stringify(err),
+    };
   }
 
-  return { statusCode: 200, body: 'Disconnected.' };
-};
+  return { statusCode: 200, body: "DisConnected." };
+}
